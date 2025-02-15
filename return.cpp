8c0dd02dde8635dd983507f3e6991671cc33cdc6
YashwantKora <Yashwantqwerty@gmail.com>
2025-02-15 19:15:00 +0530

    return DONE

--- return.cpp
+++ return.cpp
@@ -2,6 +2,7 @@
 
 double Square(double length);
 double Cube(double length);
+std::string ConcatStrings(std::string string1, std::string string2);
 
 int main(){
     double length = 5.0;
@@ -10,14 +11,21 @@ int main(){
     double result1 = Cube(length);
     std::cout << "Square: " << result << std::endl;
     std::cout << "Cube: " << result1 << std::endl;
+
+    std::string firstName = "Yashwant";
+    std::string lastName = "Kora";
+    std::string fullName = ConcatStrings(firstName, lastName);
+    std::cout << "Your Full name is: " << fullName << std::endl;
     return 0;
 }
 
 double Square(double length){
     return length * length;
-
 }
-
 double Cube(double length){
     return length * length * length;
 }
+std::string ConcatStrings(std::string firstName, std::string lastName){
+    return firstName + ' ' + lastName;
+}
+
